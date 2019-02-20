8fa33ae5ee98bfb8bfd60316e9bf183651e5d386
Kirsten Werner <kirstenmwerner@gmail.com>
2019-02-20 20:48:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School 
+  
+  attr_reader :name, :roster
+  
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end 
+  
+  def add_student(name, grade_level)
+    if roster[grade_level] == nil  
+      roster[grade_level] = [name]
+    else roster[grade_level] << name 
+    end 
+  end 
+  
+  def grade(grade_level)
+    roster[grade_level]
+  end 
+  
+  def sort
+    roster.values.sort 
+  end 
+
+end 
